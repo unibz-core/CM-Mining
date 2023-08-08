565ae16c73858cb33d5597853fa1aab615c3f184
pedropaulofb <pedropaulofb@gmail.com>
2023-08-08 19:26:21 +0200

    created docstrings

--- ontouml2networkx/generateinput.py
+++ ontouml2networkx/generateinput.py
@@ -1,3 +1,9 @@
+"""
+This module provides a set of functions for processing NetworkX graphs by removing nodes and edges with specific labels
+and saving the processed graphs to various formats. It also offers functions for replacing specific node and edge
+labels with default labels and flattening nested lists.
+"""
+
 import os
 import pickle
 
@@ -11,17 +17,46 @@ for filename in os.listdir(directory_path):
         file_names.append(filename)
 
 
-def remove_nodes_with_label(graph, label):
+def remove_nodes_with_label(graph, label)->None:
+    """
+    Remove nodes from a graph with a specific label.
+
+    :param graph: The NetworkX graph.
+    :type graph: nx.Graph
+    :param label: The label of nodes to be removed.
+    :type label: str
+    """
     nodes_to_remove = [node for node, data in graph.nodes(data=True) if data.get('label') == label]
     graph.remove_nodes_from(nodes_to_remove)
 
 
-def remove_edges_with_label(graph, label):
+def remove_edges_with_label(graph, label)->None:
+    """
+    Remove edges from a graph with a specific label.
+
+    :param graph: The NetworkX graph.
+    :type graph: nx.Graph
+    :param label: The label of edges to be removed.
+    :type label: str
+    """
     edges_to_remove = [(u, v) for u, v, data in graph.edges(data=True) if data.get('label') == label]
     graph.remove_edges_from(edges_to_remove)
 
 
 def process_graphs(node_labels, edge_labels, graphs):
+    """
+    Process a list of graphs by removing nodes and edges with specific labels.
+
+    :param node_labels: List of node labels to be removed.
+    :type node_labels: list of str
+    :param edge_labels: List of edge labels to be removed.
+    :type edge_labels: list of str
+    :param graphs: List of graph data as tuples containing a name and a NetworkX graph.
+    :type graphs: list of tuple
+    :return: List of processed NetworkX graphs.
+    :rtype: list of nx.Graph
+    """
+
     undirectedgraphs_f = []
 
     for [i, graph] in graphs:
@@ -37,6 +72,19 @@ def process_graphs(node_labels, edge_labels, graphs):
 
 
 def process_graphs_with_names(node_labels, edge_labels, graphs):
+    """
+    Process a list of graphs by removing nodes and edges with specific labels, preserving graph names.
+
+    :param node_labels: List of node labels to be removed.
+    :type node_labels: list of str
+    :param edge_labels: List of edge labels to be removed.
+    :type edge_labels: list of str
+    :param graphs: List of graph data as tuples containing a name and a NetworkX graph.
+    :type graphs: list of tuple
+    :return: List of processed graph data with names and NetworkX graphs.
+    :rtype: list of tuple
+    """
+
     undirectedgraphs_f = []
 
     for [i, graph] in graphs:
@@ -52,6 +100,19 @@ def process_graphs_with_names(node_labels, edge_labels, graphs):
 
 
 def process_graphs_(node_labels, edge_labels, graphs):
+    """
+    Process a list of graphs by removing nodes and edges with specific labels, preserving additional data.
+
+    :param node_labels: List of node labels to be removed.
+    :type node_labels: list of str
+    :param edge_labels: List of edge labels to be removed.
+    :type edge_labels: list of str
+    :param graphs: List of graph data as tuples containing support, index, and a NetworkX graph.
+    :type graphs: list of tuple
+    :return: List of processed graph data with support, index, and NetworkX graphs.
+    :rtype: list of tuple
+    """
+
     undirectedgraphs_f = []
 
     for [support, index, graph] in graphs:
@@ -67,6 +128,19 @@ def process_graphs_(node_labels, edge_labels, graphs):
 
 
 def process_graphs__(node_labels, edge_labels, graphs):
+    """
+    Process a list of graphs by removing nodes and edges with specific labels, preserving index dictionary.
+
+    :param node_labels: List of node labels to be removed.
+    :type node_labels: list of str
+    :param edge_labels: List of edge labels to be removed.
+    :type edge_labels: list of str
+    :param graphs: List of graph data as tuples containing index dictionary and a NetworkX graph.
+    :type graphs: list of tuple
+    :return: List of processed graph data with index dictionary and NetworkX graphs.
+    :rtype: list of tuple
+    """
+
     undirectedgraphs_f = []
 
     for [index_dict, graph] in graphs:
@@ -82,6 +156,21 @@ def process_graphs__(node_labels, edge_labels, graphs):
 
 
 def replace_labels_with_default(class_labels, relation_labels, edge_labels, graphs):
+    """
+    Replace specific node and edge labels with default labels in a list of graphs.
+
+    :param class_labels: List of class labels to be replaced.
+    :type class_labels: list of str
+    :param relation_labels: List of relation labels to be replaced.
+    :type relation_labels: list of str
+    :param edge_labels: List of edge labels to be removed.
+    :type edge_labels: list of str
+    :param graphs: List of NetworkX graphs to be processed.
+    :type graphs: list of nx.Graph
+    :return: List of processed NetworkX graphs.
+    :rtype: list of nx.Graph
+    """
+
     # Create a mapping for node and edge labels
     class_mapping = {label: "class" for label in class_labels}
     relation_mapping = {label: "relation" for label in relation_labels}
@@ -99,13 +188,14 @@ def replace_labels_with_default(class_labels, relation_labels, edge_labels, grap
     return graphs
 
 
-def save_graphs_to_pickle(graphs, filename):
+def save_graphs_to_pickle(graphs, filename)->None:
     """
     Save a list of NetworkX graphs to a pickle file in a specific folder.
     
-    Args:
-        graphs (list): List of NetworkX graphs.
-        filename (str): Name of the pickle file including the folder path.
+    :param graphs: List of NetworkX graphs.
+    :type graphs: list of nx.Graph
+    :param filename: Name of the pickle file including the folder path.
+    :type filename: str
     """
     folder_path = os.path.dirname(filename)
 
@@ -118,13 +208,29 @@ def save_graphs_to_pickle(graphs, filename):
 
 
 def flatten(lst):
+    """
+    Flatten a nested list into a single list.
+
+    :param lst: The nested list to be flattened.
+    :type lst: list
+    :return: The flattened list.
+    :rtype: list
+    """
     if isinstance(lst, list):
         return [elem for sublist in lst for elem in flatten(sublist)]
     else:
         return [lst]
 
 
-def graphs_to_data_file(graphs, name):
+def graphs_to_data_file(graphs, name)->None:
+    """
+    Write graph data to a data file in the specified format.
+
+    :param graphs: List of graph data as tuples containing a name and a NetworkX graph.
+    :type graphs: list of tuple
+    :param name: Name of the data file.
+    :type name: str
+    """
     with open("input/" + name + ".data", "w") as f:
         # Write the graphs
         for i, [n, G] in enumerate(graphs):
