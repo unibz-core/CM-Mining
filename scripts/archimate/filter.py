44a15b286306cfd524b15fe897ef39984024e29b
plglaser <plg741@gmail.com>
2024-03-03 18:55:55 +0100

    move archimate types into separate file

--- scripts/archimate/filter.py
+++ scripts/archimate/filter.py
@@ -1,173 +1,6 @@
 from examples import custom_style_2
 from PyInquirer import prompt, Separator
-
-'''
-See ArchiMate Specification: 
-https://pubs.opengroup.org/architecture/archimate32-doc/ch-Summary-of-Language-Notation.html
-'''
-
-business_types = {
-    'BusinessActor',
-    'BusinessRole',
-    'BusinessCollaboration',
-    'BusinessInterface',
-    'BusinessProcess',
-    'BusinessFunction',
-    'BusinessInteraction',
-    'BusinessService',
-    'BusinessEvent',
-    'BusinessObject',
-    'Contract',
-    'Representation',
-    'Product'
-}
-application_types = {
-    'ApplicationComponent',
-    'ApplicationCollaboration',
-    'ApplicationInterface',
-    'ApplicationProcess',
-    'ApplicationFunction',
-    'ApplicationInteraction',
-    'ApplicationService',
-    'ApplicationEvent',
-    'DataObject'
-}
-technology_types = {
-    'Facility',
-    'Equipment',
-    'Material',
-    'Node',
-    'Device',
-    'SystemSoftware',
-    'TechnologyCollaboration',
-    'TechnologyInterface',
-    'TechnologyProcess',
-    'TechnologyFunction',
-    'TechnologyInteraction',
-    'TechnologyService',
-    'TechnologyEvent',
-    'Artifact',
-    'CommunicationNetwork',
-    'Path',
-    'DistributionNetwork'
-}
-motivation_types = {
-    'Stakeholder',
-    'Driver',
-    'Assessment',
-    'Goal',
-    'Outcome',
-    'Principle',
-    'Requirement',
-    'Constraint',
-    'Value',
-    'Meaning'
-}
-strategy_types = {
-    'Resource', # structure element, but neither active nor passive
-    'Capability',
-    'ValueStream',
-    'CourseOfAction'
-}
-implementation_migration_types = {
-    'WorkPackage',
-    'ImplementationEvent',
-    'Deliverable',
-    'Plateau',
-    'Gap'
-}
-other_types = {
-    'Location',
-    'Grouping',
-    'Junction',
-    'OrJunction',
-    'AndJunction'
-}
-relationship_types = {
-    'Association',
-    'Serving',
-    'Flow',
-    'Realization',
-    'Aggregation',
-    'Influence',
-    'Composition',
-    'Triggering',
-    'Assignment',
-    'Specialization',
-    'Access'
-}
-layer_to_types = {
-    "Business": business_types,
-    "Application": application_types,
-    "Technology": technology_types,
-    "Motivation": motivation_types,
-    "Strategy": strategy_types,
-    "Implementation & Migration": implementation_migration_types,
-    "Other": other_types
-}
-aspects = {
-    'Active Structure': {
-        'BusinessActor', 
-        'BusinessRole', 
-        'BusinessCollaboration',
-        'BusinessInterface',
-        'ApplicationComponent',
-        'ApplicationCollaboration',
-        'ApplicationInterface',
-        'Node',
-        'Device',
-        'SystemSoftware',
-        'TechnologyCollaboration',
-        'TechnologyInterface',
-        'CommunicationNetwork',
-        'Path',
-        'Facility',
-        'Equipment',
-        'DistributionNetwork',
-    },
-    'Behavior': {
-        'BusinessProcess',
-        'BusinessFunction',
-        'BusinessInteraction',
-        'BusinessService',
-        'BusinessEvent',
-        'ApplicationProcess',
-        'ApplicationFunction',
-        'ApplicationInteraction',
-        'ApplicationService',
-        'ApplicationEvent',
-        'TechnologyProcess',
-        'TechnologyFunction',
-        'TechnologyInteraction',
-        'TechnologyService',
-        'TechnologyEvent',
-        'Capability',
-        'ValueStream',
-        'CourseOfAction',
-        'WorkPackage',
-        'ImplementationEvent',
-    },
-    'Passive Structure': {
-        'BusinessObject',
-        'Contract',
-        'Representation',
-        'DataObject'
-        'Artifact',
-        'Material',
-        'Deliverable',
-        'Gap'
-    },
-    'Other': {
-        'Product'  # composite
-        'Plateau', # composite
-        'Location',
-        'Grouping',
-        'Junction',
-        'OrJunction',
-        'AndJunction'
-    },
-    'Motivation': motivation_types
-}
+import archimate.types
 
 def select_element_filter_kind():
     choices = ["Layers", "Aspects", "Specific Types"]
@@ -189,8 +22,9 @@ def select_element_filter_kind():
     print("Selected element filter kind:", selected_choice)
     return selected_choice
 
+
 def filter_layers():
-    layers = sorted(layer_to_types.keys())
+    layers = sorted(archimate.types.layer_to_types.keys())
     choices = [{'name': layer} for layer in layers]
     question = [
         {
@@ -204,9 +38,10 @@ def filter_layers():
     selected_choices = answers['selected_layers']
     print("Selected layers to filter:", selected_choices)
 
-    filtered_types = [t for choice in selected_choices for t in layer_to_types[choice]]
+    filtered_types = [t for choice in selected_choices for t in archimate.types.layer_to_types[choice]]
     print("Filtered element types:", filtered_types)
     return filtered_types
+
 
 def filter_aspects():
     strings = ["Active Structure", "Passive Structure", "Behavior", "Other", "Motivation"]
@@ -226,21 +61,22 @@ def filter_aspects():
     answers = prompt(question, style=custom_style_2)
     selected_choices = answers['selected_strings']
     print("Selected aspect filters:", selected_choices)
-    filtered_types = [t for choice in selected_choices for t in aspects[choice]]
+    filtered_types = [t for choice in selected_choices for t in archimate.types.aspects[choice]]
     print("Filtered element types:", filtered_types)
     return filtered_types
 
 
 def filter_element_types():
-    all_element_types = list(business_types.union(
-        application_types, 
-        technology_types, 
-        motivation_types, 
-        strategy_types, 
-        implementation_migration_types, 
-        other_types
-    ))
-    choices = [{'name': type_name} for type_name in sorted(all_element_types)]
+    all_element_types = sorted(list(archimate.types.business_types.union(
+        archimate.types.application_types, 
+        archimate.types.technology_types, 
+        archimate.types.physical_types,
+        archimate.types.motivation_types, 
+        archimate.types.strategy_types, 
+        archimate.types.implementation_migration_types, 
+        archimate.types.other_types
+    )))
+    choices = [{'name': type_name} for type_name in all_element_types]
     question = [
         {
             'type': 'checkbox',
@@ -255,8 +91,10 @@ def filter_element_types():
     print("Selected element type filters:", selected_choices)
     return selected_choices
 
+
 def filter_relationship_types():
-    choices = [{'name': type_name} for type_name in sorted(list(relationship_types))]
+    rel_types = sorted(list(archimate.types.relationship_types))
+    choices = [{'name': type_name} for type_name in rel_types]
     question = [
         {
             'type': 'checkbox',
@@ -269,6 +107,7 @@ def filter_relationship_types():
     selected_choices = answers['selected_types']
     print("Selected relationship type filters:", selected_choices)
     return selected_choices
+
 
 def filter_edge_labels():
     strings = ["source", "target", "specific", "general"]
--- scripts/archimate/transform.py
+++ scripts/archimate/transform.py
@@ -1,6 +1,72 @@
 import os, pickle
 import networkx as nx
 from pathlib import Path
+import archimate.types
+
+
+def clean_graphs(graphs: list[nx.Graph]) -> list[nx.Graph]:
+    rel_types = archimate.types.relationship_types
+    special_source_types = ['source', ] # add general/specific
+    special_target_types = ['target', ] # add general/specific
+    cleaned_graphs = []
+
+    for graph in graphs:
+        new_graph = nx.Graph()
+        intermediate_edges = []
+        # id: 1 
+        # type: Composition
+        # source 
+
+        for node, data in graph.nodes(data=True):
+            if data['label'] not in rel_types:
+                new_graph.add_node(node, **data)
+
+        for node, data in graph.nodes(data=True):
+            if data['label'] in rel_types:
+                source_target_edges = graph.edges(node, data=True)
+                if len(source_target_edges) != 2:
+                    continue
+                
+                source = None
+                target = None
+                for u, v, data2 in source_target_edges:
+                    if data2['relation'] in special_source_types:
+                        source = v
+                    if data2['relation'] in special_target_types:
+                        target = v
+                
+                if source is None or target is None:
+                    continue
+                
+                new_graph.add_edge(source, target, **data)
+
+        cleaned_graphs.append(new_graph)
+    
+    return cleaned_graphs
+
+
+def convert_to_graph(patterns_file: Path) -> list[nx.Graph]:
+    graphs = []
+    current_graph = None
+
+    with open(patterns_file, 'r') as file:
+        for line in file.readlines():
+            parts = line.strip().split(' ')
+            
+            # start of a new graph
+            if parts[0] == 't':  
+                if current_graph is not None:
+                    graphs.append(current_graph)
+                current_graph = nx.Graph()
+            elif parts[0] == 'v' and current_graph is not None:
+                current_graph.add_node(int(parts[1]), label=parts[2])
+            elif parts[0] == 'e' and current_graph is not None:
+                current_graph.add_edge(int(parts[1]), int(parts[2]), relation=parts[3])
+    # add last processed graph
+    if current_graph not in graphs:
+        graphs.append(current_graph)
+
+    return graphs
 
 def create_graphs(models: list[dict], output_dir: Path) -> list[list]:
     graphs = []
